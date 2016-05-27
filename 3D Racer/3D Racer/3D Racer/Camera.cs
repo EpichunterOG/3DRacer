5605f9f3be5032c1229d4e3203dac62c8c252920
EpichunterOG <im.epichunter12@gmail.com>
2016-05-27 08:48:37 -0400

    Add files via upload

---
+++ 3D Racer/3D Racer/3D Racer/Camera.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace _3D_Racer
+
+{
+    class Camera
+    {
+
+        public Vector2 pos;
+        public Vector2 speed;
+        public Vector2 dir;
+        public Vector2 plane;
+        public float acceleration { get; set; }
+        public float rotation { get; set; }
+        float rAccr = 1f;
+        private float dt;
+        KeyboardState kb;
+        MouseState ms, oldms;
+
+        public Camera(Vector2 pos, Vector2 direction, Vector2 plane, float acceleration, float rotation)
+        {
+            this.pos = pos;
+            this.dir = direction;
+            this.plane = plane;
+            this.acceleration = acceleration;
+            this.rotation = rotation;
+            speed = new Vector2(0f, 0f);
+
+        }
+        public void update(int[,] map, GameTime time)
+        {
+            dt = time.ElapsedGameTime.Milliseconds / 1000f;
+
+            kb = Keyboard.GetState();
+            ms = Mouse.GetState();
+            #region keyboard
+            /*
+            if (kb.IsKeyDown(Keys.W))
+            {
+                if (map[(int)(pos.X + dir.X * acceleration * dt), (int)pos.Y] == 0)
+                {
+                    pos.X += dir.X * acceleration * dt;
+                }
+                if (map[(int)(pos.X), (int)(pos.Y + dir.Y * acceleration * dt)] == 0)
+                {
+                    pos.Y += dir.Y * acceleration * dt;
+                }
+            }
+             * */
+            if (kb.IsKeyDown(Keys.W))
+            {
+                
+                    speed.X += dir.X * acceleration * dt;
+                
+               
+                    speed.Y += dir.Y * acceleration * dt;
+                
+            }
+            if(kb.IsKeyDown(Keys.L))
+                rAccr=2.5f;
+            else
+                rAccr=1f;
+            /*
+            if (kb.IsKeyDown(Keys.S))
+            {
+                if (map[(int)(pos.X - dir.X * acceleration * dt), (int)pos.Y] == 0)
+                {
+                    pos.X -= dir.X * acceleration * dt;
+                }
+                if (map[(int)(pos.X), (int)(pos.Y - dir.Y * acceleration * dt)] == 0)
+                {
+                    pos.Y -= dir.Y * acceleration * dt;
+                }
+            }*/
+
+            if (map[(int)(pos.X + speed.X), (int)pos.Y] == 0)
+            {
+                pos.X += speed.X;
+            }
+            else
+            {
+                
+            }
+            if (map[(int)(pos.X), (int)(pos.Y + speed.Y)] == 0)
+            {
+                pos.Y += speed.Y;
+            }
+
+            if (kb.IsKeyDown(Keys.D))
+            {
+                float oldxDir = dir.X;
+                dir.X = (float)(dir.X * Math.Cos(-rotation * dt*rAccr) - dir.Y * Math.Sin(-rotation * dt*rAccr));
+                dir.Y = (float)(oldxDir * Math.Sin(-rotation * dt*rAccr) + dir.Y * Math.Cos(-rotation * dt*rAccr));
+                float oldxPlane = plane.X;
+                plane.X = (float)(plane.X * Math.Cos(-rotation * dt*rAccr) - plane.Y * Math.Sin(-rotation * dt*rAccr));
+                plane.Y = (float)(oldxPlane * Math.Sin(-rotation * dt*rAccr) + plane.Y * Math.Cos(-rotation * dt*rAccr));
+            }
+            if (kb.IsKeyDown(Keys.A))
+            {
+
+                float oldxDir = dir.X;
+                dir.X = (float)(dir.X * Math.Cos(rotation * dt*rAccr) - dir.Y * Math.Sin(rotation * dt*rAccr));
+                dir.Y = (float)(oldxDir * Math.Sin(rotation * dt*rAccr) + dir.Y * Math.Cos(rotation * dt*rAccr));
+                float oldxPlane = plane.X;
+                plane.X = (float)(plane.X * Math.Cos(rotation * dt*rAccr) - plane.Y * Math.Sin(rotation * dt*rAccr));
+                plane.Y = (float)(oldxPlane * Math.Sin(rotation * dt*rAccr) + plane.Y * Math.Cos(rotation * dt*rAccr));
+
+            }
+            #endregion
+            #region mouse
+            
+            if (ms.X != oldms.X)
+            {
+                float dx = oldms.X - ms.X;
+
+                float oldxDir = dir.X;
+                dir.X = (float)(dir.X * Math.Cos(rotation * dx * dt) - dir.Y * Math.Sin(rotation * dx * dt));
+                dir.Y = (float)(oldxDir * Math.Sin(rotation * dx * dt) + dir.Y * Math.Cos(rotation * dx * dt));
+                float oldxPlane = plane.X;
+                plane.X = (float)(plane.X * Math.Cos(rotation * dx * dt) - plane.Y * Math.Sin(rotation * dx * dt));
+                plane.Y = (float)(oldxPlane * Math.Sin(rotation * dx * dt) + plane.Y * Math.Cos(rotation * dx * dt));
+
+            }
+
+            oldms = ms;
+
+
+
+           
+
+
+            #endregion
+        }
+
+    }
+}
---
+++ 3D Racer/3D Racer/3D Racer/Screen.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _3D_Racer
+
+{
+    class Screen
+    {
+
+
+        int w, h;
+        int[,] map;
+        List<Texture2D> textures;
+        List<Color[]> texPix;
+        Color lessBlack = new Color(30, 30, 30);
+        #region tempVars
+        double cameraX, rayDirX, rayDirY, sideDistX, sideDistY,wallX, deltaDistX,deltaDistY, perpWallDist;
+        int mapX, mapY,stepX,stepY,lineHeight,texNum,texX,texY,drawStart,drawEnd;
+        bool hit;
+        Color color;
+        #endregion
+        public Screen(int[,] map, List<Texture2D> textures, int w, int h)
+        {
+
+            this.map = map;
+            this.textures = textures;
+            this.w = w;
+            this.h = h;
+            texPix = new List<Color[]>();
+
+            for(int i = 0; i<textures.Count;i++)
+            texPix.Add(new Color[textures[0].Width*textures[0].Height]);
+            
+            for (int i = 0; i < textures.Count; i++)
+            {
+                textures[i].GetData<Color>(0, null, texPix[i], 0, textures[i].Width * textures[i].Height);
+            }
+        }
+        public void update(Camera cam, Color[] pixels)
+        {
+            //clear the screen (top part is dark gray, bottom party is gray 
+            //to make the top look like a celing and bottom like a floor)
+            for (int n = 0; n < pixels.Length / 2; n++)
+            {
+                if (pixels[n] != lessBlack)
+                    pixels[n] = lessBlack;
+            }
+            for (int i = pixels.Length / 2; i < pixels.Length; i++)
+            {
+                if (pixels[i] != Color.Black)
+                    pixels[i] = Color.Black;
+            }
+            for (int x = 0; x < w; x++)
+            {
+                 cameraX = 2 * x / (double)(w) - 1;
+                 rayDirX = cam.dir.X + cam.plane.X * cameraX;
+                 rayDirY = cam.dir.Y + cam.plane.Y * cameraX;
+                // Map position
+                 mapX = (int)cam.pos.X;
+                 mapY = (int)cam.pos.Y;
+                // length of ray from current position to next x or y-side
+               // double sideDistX;
+               // double sideDistY;
+                // Length of ray from one side to next in map
+                 deltaDistX = Math.Sqrt(1 + (rayDirY * rayDirY)
+                        / (rayDirX * rayDirX));
+                 deltaDistY = Math.Sqrt(1 + (rayDirX * rayDirX)
+                        / (rayDirY * rayDirY));
+                //double perpWallDist;
+                // Direction to go in x and y
+               // int stepX, stepY;
+                 hit = false;// was a wall hit
+                int side = 0;// was the wall vertical or horizontal
+                // Figure out the step direction and initial distance to a side
+                if (rayDirX < 0)
+                {
+                    stepX = -1;
+                    sideDistX = (cam.pos.X - mapX) * deltaDistX;
+                }
+                else
+                {
+                    stepX = 1;
+                    sideDistX = (mapX + 1.0 - cam.pos.X) * deltaDistX;
+                }
+                if (rayDirY < 0)
+                {
+                    stepY = -1;
+                    sideDistY = (cam.pos.Y - mapY) * deltaDistY;
+                }
+                else
+                {
+                    stepY = 1;
+                    sideDistY = (mapY + 1.0 - cam.pos.Y) * deltaDistY;
+                }
+                // Loop to find where the ray hits a wall
+                while (!hit)
+                {
+                    // Jump to next square
+                    if (sideDistX < sideDistY)
+                    {
+                        sideDistX += deltaDistX;
+                        mapX += stepX;
+                        side = 0;
+                    }
+                    else
+                    {
+                        sideDistY += deltaDistY;
+                        mapY += stepY;
+                        side = 1;
+                    }
+                    // Check if ray has hit a wall
+                    // System.out.println(mapX + ", " + mapY + ", " +
+                    // map[mapX][mapY]);
+                    if (map[mapX, mapY] > 0)
+                        hit = true;
+                }
+                // Calculate distance to the point of impact
+                if (side == 0)
+                    perpWallDist = Math.Abs((mapX - cam.pos.X + (1 - stepX) / 2)
+                            / rayDirX);
+                else
+                    perpWallDist = Math.Abs((mapY - cam.pos.Y + (1 - stepY) / 2)
+                            / rayDirY);
+                // Now calculate the height of the wall based on the distance from
+                // the camera
+                //int lineHeight;
+                if (perpWallDist > 0)
+                    lineHeight = (int)(h / perpWallDist);
+                else
+                    lineHeight = h;
+                // calculate lowest and highest pixel to fill in current stripe
+                 drawStart = -lineHeight/2  + h / 2;
+                if (drawStart < 0)
+                    drawStart = 0;
+                 drawEnd = lineHeight/2  + h / 2;
+                if (drawEnd >= h)
+                    drawEnd = h - 1;
+                // add a texture
+                 texNum = map[mapX, mapY] - 1;
+                //double wallX;// Exact position of where wall was hit
+                if (side == 1)
+                {// If its a y-axis wall
+                    wallX = (cam.pos.X + ((mapY - cam.pos.Y + (1 - stepY) / 2) / rayDirY)
+                            * rayDirX);
+                }
+                else
+                {// X-axis wall
+                    wallX = (cam.pos.Y + ((mapX - cam.pos.X + (1 - stepX) / 2) / rayDirX)
+                            * rayDirY);
+                }
+                wallX -= Math.Floor(wallX);
+                // x coordinate on the texture
+                 texX = (int)(wallX * (textures[texNum].Width));
+                if (side == 0 && rayDirX > 0)
+                    texX = textures[texNum].Width - texX - 1;
+                else if (side == 1 && rayDirY < 0)
+                    texX = textures[texNum].Width - texX - 1;
+                // calculate y coordinate on texture
+                for (int y = drawStart; y < drawEnd; y++)
+                {
+                     texY = (((y * 2 - h + lineHeight) << 6) / lineHeight) / 2;
+                    Color color;
+                  
+                        color = texPix[texNum][texX
+                                + (texY * textures[texNum].Width)];
+ 
+                    pixels[x + y * (w)] = color;
+                   
+                }
+            }
+        }
+    }
+}
